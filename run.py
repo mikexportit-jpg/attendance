8efadd676f85ee667e99fb73a914c584b8c9b5ae
ibrahim <mikexportit@gmail.com>
2025-12-17 15:58:31 +0200

    Initial commit

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ def init_db():
         print("Database already exists.")
 
 if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
     with app.app_context():
         db.create_all()
     app.run(debug=True)
